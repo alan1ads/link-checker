fddad7f7bd0d9f0b3a607412307e04387f0fe769
alan1ads <alan@adsyncmedia.com>
2025-03-04 19:01:31 -0500

    done testing working ignoring the corresponding

--- linkchecker PUBLIC.py
+++ linkchecker PUBLIC.py
@@ -262,9 +262,6 @@ async def check_links():
             domain_data = [(account, 'http://' + domain if not domain.startswith(('http://', 'https://')) else domain) 
                           for account, domain in domain_data]
             
-            # Limit to first 20 domains for testing
-            domain_data = domain_data[:20]
-            
             failing_domains = []
             checked_count = 0
             
